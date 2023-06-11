16338378b5f407cec4667ff0500f64cdc4b75391
utsav-galphat <utsav.galphat97@gmail.com>
2023-06-11 22:19:58 +0530

    circuitbreaker, timeout, retry added with resilience4j

--- inventory-service/src/main/java/com/utech/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/utech/inventoryservice/service/InventoryService.java
@@ -3,6 +3,7 @@ package com.utech.inventoryservice.service;
 import com.utech.inventoryservice.dto.InventoryResponse;
 import com.utech.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,8 +19,12 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows  //for resilience timeout
     public List<InventoryResponse> isInStock(List<String> skuCode) {
-        log.info("Checking Inventory");
+//        log.info("Checking Inventory");
+//        Thread.sleep(10000);
+//        log.info("In Stock");
+
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                         InventoryResponse.builder()
